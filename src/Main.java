fa23eddbe102ce19b795d08eea6d8c18a6982a62
MakeevAlexandr <pipifax@bk.ru>
2022-07-04 13:56:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 14750; // price ticket
+        int y = 20; // number of rubles per one bonus mile
+        int z = x/y; //  number bonus miles
+        System.out.println ("Number of miles accrued for the purchased ticket: " + z);
+    }
+}
